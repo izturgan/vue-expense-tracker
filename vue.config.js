3a024cc3ee5501deb5383c6f71e122492ca5b12c
Nurgissa <tasnur2002@gmail.com>
2024-04-01 15:54:50 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-expense-tracker/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-expense-tracker/" : "/",
+};
